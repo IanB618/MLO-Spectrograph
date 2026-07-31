b71371dfa385952f5fa5da73505175996fea3705
Caden Gobat <36030084+cgobat@users.noreply.github.com>
2026-07-30 17:56:43 -0700

    Drop ROWORDER keyword from FITS headers

--- ics/src/metadata_utils.py
+++ ics/src/metadata_utils.py
@@ -22,16 +22,17 @@ def get_cpu_temp():
 
 def update_fits_metadata(filepath: Path, request: ExposureRequest):
     with fits.open(filepath, mode="update") as f:
-        try:
-            _ = f[0].header.pop("COMMENT")
-        except KeyError:
-            pass
+        for kw in ["COMMENT", "ROWORDER"]:
+            try:
+                _ = f[0].header.pop(kw)
+            except KeyError:
+                pass
         date_obs = Time(f[0].header.get("DATE-OBS"), format="fits")
         f[0].header.set("MJD-OBS", date_obs.mjd, "MJD start of observation", after="DATE-OBS")
         f[0].header.set("TELESCOP", "Claud 1.25-m Telescope")
         camera = f[0].header.get("INSTRUME", None)
         f[0].header.set("INSTRUME", "Fiber-Fed Spectrograph", "Instrument name", after="TELESCOP")
-        f[0].header.set("CAMERA", camera, "Active camera", after="INSTRUME")
+        f[0].header.set("CAMERA", camera, "Camera name", after="INSTRUME")
         f[0].header.set("OBJECT", request.object_name, "Target name", after="CAMERA")
         f[0].header.set("CPU-TEMP", get_cpu_temp(), "[degC] Instrument computer processor temperature", after="CCD-TEMP")
         f[0].header.set("DATE", Time.now().isot, "Time HDU was created/modified")
