77bebcaaa8cc7cd2c722f4eb00ce7d4754436311
Caden Gobat <36030084+cgobat@users.noreply.github.com>
2026-08-03 09:19:42 -0700

    Make header construction more robust; add HDU checksum

--- ics/src/metadata_utils.py
+++ ics/src/metadata_utils.py
@@ -38,21 +38,19 @@ def update_fits_metadata(request: ExposureRequest, system_status: SystemSnapshot
                 _ = f[0].header.pop(kw)
             except KeyError:
                 pass
+            f[0].header.set("BSCALE", f[0].header.get("BSCALE", 1), after="EXTEND")
+            if "BZERO" in f[0].header: # BZERO should come before BSCALE if present
+                f[0].header.set("BZERO", after="EXTEND")
 
-        date_obs = Time(f[0].header.get("DATE-OBS"), format="fits", location=MLO)
-        f[0].header.set("MJD-OBS", date_obs.mjd, "MJD start of observation", after="DATE-OBS")
-        f[0].header.set("SIDEREAL", date_obs.sidereal_time("mean").to_string(sep=":", precision=1).zfill(10),
-                        "Mean sidereal time at start of observation", after="MJD-OBS")
-
-        f[0].header.set("TELESCOP", "Claud 1.25-m Telescope")
-        f[0].header.set("LOCATION", "Mt Laguna Observatory", "Observatory name", before="TELESCOP")
+        f[0].header.set("LOCATION", "Mt Laguna Observatory", "Observatory name", after="BSCALE")
+        f[0].header.set("TELESCOP", "Claud 1.25-m Telescope", "Telescope name", after="LOCATION")
         camera = f[0].header.get("INSTRUME", None)
         f[0].header.set("INSTRUME", "Fiber-Fed Spectrograph", "Instrument name", after="TELESCOP")
-        f[0].header.set("CAMERA", camera, "Camera name", after="INSTRUME")
+        f[0].header.set("CAMERA", camera, "Camera name (from INDI)", after="INSTRUME")
         f[0].header.set("FILTER", "ThorLabs FGL400S", "ID of filter in use", after="CAMERA") # TODO
         f[0].header.set("GRATING", "Newport 270R", "ID of grating in use", after="FILTER") # TODO
 
-        f[0].header.set("OBJECT", request.object_name, "Target name", after="CAMERA")
+        f[0].header.set("OBJECT", request.object_name, "Target name", after="GRATING")
         f[0].header.set("RA", tcs_status.ra, "[deg] Nominal right ascension", after="OBJECT")
         f[0].header.set("DEC", tcs_status.dec, "[deg] Nominal declination", after="RA")
         f[0].header.set("AIRMASS", tcs_status.airmass, "Airmass at end of observation", after="DEC")
@@ -65,7 +63,13 @@ def update_fits_metadata(request: ExposureRequest, system_status: SystemSnapshot
         f[0].header.rename_keyword("FOCUSPOS", "CAMFOCUS")
         f[0].header.set("TELFOCUS", None, "Telescope focus position", before="CAMFOCUS") # TODO: tcs_status.focus_position
 
+        date_obs = Time(f[0].header.get("DATE-OBS"), format="fits", location=MLO)
+        f[0].header.set("MJD-OBS", round(date_obs.mjd, 6), "MJD start of observation", after="DATE-OBS")
+        f[0].header.set("SIDEREAL", date_obs.sidereal_time("mean").to_string(sep=":", precision=1).zfill(10),
+                        "Mean sidereal time at start of observation", after="MJD-OBS")
+
         f[0].header.set("BOX-TEMP", None, "[degC] Instrument enclosure ambient temperature", after="CCD-TEMP") # TODO
         f[0].header.set("CPU-TEMP", get_cpu_temp(), "[degC] Instrument computer processor temperature", after="BOX-TEMP")
         f[0].header.set("TECPOWER", system_status.science_camera.cooler_power_pct, "[%] Thermoelectric cooler power", after="CCD-TEMP")
         f[0].header.set("DATE", Time.now().isot, "Time HDU was created/modified")
+        f[0].add_checksum()
